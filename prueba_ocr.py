469d670e6cfa36ae150b6d0656d6c710a4639459
Omar Gabriel Fazzito <ofazzito@yahoo.com.ar>
2021-06-02 23:50:17 -0300

    agrego OCR

---
+++ prueba_ocr.py
@@ -0,0 +1,10 @@
+from alpr.alpr import ALPR
+import cv2
+import yaml
+
+im = cv2.imread('data/imgs/prueba.jpg')
+with open('config.yaml', 'r') as stream:
+    cfg = yaml.safe_load(stream)
+alpr = ALPR(cfg['modelo'], cfg['db'])
+predicciones = alpr.predict(im)
+print("Prueba: ", predicciones)
