cf063c147d7e12e8e7b2070bd8de4574a50ce0d6
Omar Gabriel Fazzito <ofazzito@yahoo.com.ar>
2021-06-03 00:14:22 -0300

    Agrego configuracion y datos de prueba para OCR

--- prueba_ocr.py
+++ prueba_ocr.py
@@ -2,7 +2,7 @@ from alpr.alpr import ALPR
 import cv2
 import yaml
 
-im = cv2.imread('data/imgs/prueba.jpg')
+im = cv2.imread('data/imags/prueba.jpg')
 with open('config.yaml', 'r') as stream:
     cfg = yaml.safe_load(stream)
 alpr = ALPR(cfg['modelo'], cfg['db'])
